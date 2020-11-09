bea7ebd3562db5d31d1451225d8e588c45e91b9d
whddus9509 <whddus9509@naver.com>
2020-11-09 21:54:06 +0900

    Create BubbleSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.sorts.BubbleSort;
 import com.company.structs.LinkedList;
 import com.company.test.LinkedListTest;
 import com.company.test.StackTest;
@@ -7,7 +8,11 @@ import com.company.test.StackTest;
 public class Main {
 
     public static void main(String[] args) {
-        StackTest stackTest = new StackTest();
-        stackTest.testStack();
+        BubbleSort bubbleSort = new BubbleSort();
+        int arr[]={9,8,7,6,5,4,3,2,1};
+        bubbleSort.sort(arr);
+        for(int i : arr){
+            System.out.println(i);
+        }
     }
 }
