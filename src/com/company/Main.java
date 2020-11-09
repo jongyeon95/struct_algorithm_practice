0fa228d36180f1f7a5b28bdf41f299170eafaf9c
whddus9509 <whddus9509@naver.com>
2020-11-10 00:51:52 +0900

    Create InsertSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.sorts.BubbleSort;
+import com.company.sorts.InsertSort;
 import com.company.sorts.SelectionSort;
 import com.company.structs.LinkedList;
 import com.company.test.LinkedListTest;
@@ -10,8 +11,8 @@ public class Main {
 
     public static void main(String[] args) {
         int arr[]={9,7,4,3,5,2,1,6,8};
-        SelectionSort selectionSort= new SelectionSort();
-        selectionSort.sort(arr);
+        InsertSort insertSort= new InsertSort();
+        insertSort.sort(arr);
         for(int num : arr){
             System.out.println(num);
         }
---
+++ src/com/company/sorts/InsertSort.java
@@ -0,0 +1,18 @@
+package com.company.sorts;
+
+public class InsertSort {
+    public void sort(int[] arr){
+        for(int i=1; i<arr.length; i++){
+            for(int j=i; j>0; j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+            }
+        }
+}
